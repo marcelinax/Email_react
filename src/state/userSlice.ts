317f90d1c30beb22468c13752b649f8671418af7
Marcelina Sitko <marcelina.sitko@interia.pl>
2021-09-24 11:49:54 +0200

    usersSlice redux reducer  + User.ts type

---
+++ src/state/userSlice.ts
@@ -0,0 +1,45 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import User from "../types/User";
+
+interface UserState {
+    users: User[];
+    loggedUser: User | null;
+}
+
+const saveUsersInLocalStorage = (state: User[]): void => {
+    localStorage.setItem('emailUsers', JSON.stringify(state));
+};
+
+const loadUsersFromLocalStorage = (): User[] => {
+    return JSON.parse(localStorage.getItem('emailUsers') || '[]');
+};
+
+const initialState: UserState = {
+    users: loadUsersFromLocalStorage(),
+    loggedUser: null
+};
+
+export const usersSlice = createSlice({
+    name: 'users',
+    initialState: initialState,
+    reducers: {
+
+        loginUser: (state, action: PayloadAction<User>) => {
+            const {email} = action.payload;
+            const userIndex = state.users.map(user => user.email).indexOf(email);
+            if (state.users.map(user => user.email).includes(email)) {
+
+                state.loggedUser = state.users[userIndex];
+
+            } else {
+                state.users = [...state.users, action.payload];
+                state.loggedUser = action.payload;
+                saveUsersInLocalStorage(state.users);
+            }
+
+        }
+    }
+});
+
+export const {loginUser} = usersSlice.actions;
+export default usersSlice.reducer;
